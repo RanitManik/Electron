307a7c9fc7bccde8e79979ebf212e4db9b75e81e
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-09-23 12:04:24 +0530

    added code

--- markdown-editor/src/renderer/index.ts
+++
@@ -1,7 +0,0 @@
-import { renderMarkdown } from './markdown';
-import Elements from './elements';
-
-Elements.MarkdownView.addEventListener('input', async () => {
-  const markdown = Elements.MarkdownView.value;
-  renderMarkdown(markdown);
-});
